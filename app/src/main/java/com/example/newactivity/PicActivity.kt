5a2fe0c3f95468786a9861760f19499ab0c7a1f7
Сергей Москвин <moskvin.sergey2016@mail.ru>
2021-10-28 16:07:57 +0700

    Initial commit

---
+++ app/src/main/java/com/example/newactivity/PicActivity.kt
@@ -0,0 +1,11 @@
+package com.example.newactivity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PicActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pic_layout)
+    }
+}
